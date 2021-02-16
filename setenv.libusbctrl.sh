ad753d83dfcfdb880e0130276a74451a129d15a4
Philippe Thierry <phil@reseau-libre.net>
2021-02-16 21:15:29 +0100

    [fix] typo in EwoK include path

--- setenv.libusbctrl.sh
+++ setenv.libusbctrl.sh
@@ -6,5 +6,5 @@ set -x
 export USBOTGHS_API_DIR=../usbotghs/api
 export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
 export LIBSTD_API_DIR=../libstd/api
-export EWOK_API_DIR=../libstd/framac/include/ewok
+export EWOK_API_DIR=../libstd/framac/includes/ewok
 set +x
--- setenv.usbotghs.sh
+++ setenv.usbotghs.sh
@@ -7,5 +7,5 @@ export USBOTGHS_API_DIR=../usbotghs/api
 export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
 export LIBSTD_API_DIR=../libstd/api
 export LIBUSBCTRL_API_DIR=../libusbctrl/api
-export EWOK_API_DIR=../libstd/framac/include/ewok
+export EWOK_API_DIR=../libstd/framac/includes/ewok
 set +x
