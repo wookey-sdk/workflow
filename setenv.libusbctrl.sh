c6d5d52d21c1b80b2bdd13fe15ebcdf285e3e7e5
Philippe Thierry <phil@reseau-libre.net>
2021-02-16 21:11:10 +0100

    [update] support for two jobs (usbotghs, libusbctrl). Fix path in setenv

---
+++ setenv.libusbctrl.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "=== sourcing requested paths for Frama-C makefile"
+
+set -x
+export USBOTGHS_API_DIR=../usbotghs/api
+export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
+export LIBSTD_API_DIR=../libstd/api
+export EWOK_API_DIR=../libstd/framac/include/ewok
+set +x
--- setenv.usbotghs.sh
+++ setenv.usbotghs.sh
@@ -6,6 +6,6 @@ set -x
 export USBOTGHS_API_DIR=../usbotghs/api
 export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
 export LIBSTD_API_DIR=../libstd/api
-export LIBUSB_API_DIR=../libusbctrl/api
+export LIBUSBCTRL_API_DIR=../libusbctrl/api
 export EWOK_API_DIR=../libstd/framac/include/ewok
 set +x
