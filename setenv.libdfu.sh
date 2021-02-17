70cef94dd93799225f51aa4105419bae41066270
Philippe Thierry <phil@reseau-libre.net>
2021-02-17 09:37:50 +0100

    [fix] generated layout path

--- setenv.libdfu.sh
+++ setenv.libdfu.sh
@@ -4,7 +4,7 @@ echo "=== sourcing requested paths for Frama-C makefile"
 
 set -x
 export USBOTGHS_DIR=../usbotghs/api
-export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
+export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include
 export LIBSTD_API_DIR=../libstd/api
 export LIBUSB_DIR=../libusbctrl/api
 export EWOK_API_DIR=../libstd/framac/includes/ewok
