71585aacedf167a11b5f8021a5f352365a940793
Philippe Thierry <phil@reseau-libre.net>
2021-02-17 09:33:10 +0100

    [update] add all libs

---
+++ setenv.libdfu.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "=== sourcing requested paths for Frama-C makefile"
+
+set -x
+export USBOTGHS_DIR=../usbotghs/api
+export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
+export LIBSTD_API_DIR=../libstd/api
+export LIBUSB_DIR=../libusbctrl/api
+export EWOK_API_DIR=../libstd/framac/includes/ewok
+set +x
