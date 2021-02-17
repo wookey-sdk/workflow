b006e124dbb0b284e69b733d1c047f75b96d2d1a
Philippe Thierry <phil@reseau-libre.net>
2021-02-17 10:34:58 +0100

    [update] make framaC provers list based on target framaC version of CI

--- setenv.libdfu.sh
+++ setenv.libdfu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+FRAMAC_VERSION=`frama-c -version|cut -d'.' -f 1`;
+
 echo "=== sourcing requested paths for Frama-C makefile"
 
 set -x
@@ -8,4 +10,15 @@ export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include
 export LIBSTD_API_DIR=../libstd/api
 export LIBUSB_DIR=../libusbctrl/api
 export EWOK_API_DIR=../libstd/framac/includes/ewok
+case $FRAMAC_VERSION in
+    22)
+        export FRAMAC_WP_PROVERS=alt-ergo,z3
+        ;;
+    21)
+        export FRAMAC_WP_PROVERS=alt-ergo
+        ;;
+    *)
+        export FRAMAC_WP_PROVERS=alt-ergo
+        ;;
+esac
 set +x
--- setenv.libusbctrl.sh
+++ setenv.libusbctrl.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+FRAMAC_VERSION=`frama-c -version|cut -d'.' -f 1`;
+
 echo "=== sourcing requested paths for Frama-C makefile"
 
 set -x
@@ -7,4 +9,15 @@ export USBOTGHS_API_DIR=../usbotghs/api
 export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include/generated
 export LIBSTD_API_DIR=../libstd/api
 export EWOK_API_DIR=../libstd/framac/includes/ewok
+case $FRAMAC_VERSION in
+    22)
+        export FRAMAC_WP_PROVERS=alt-ergo,z3
+        ;;
+    21)
+        export FRAMAC_WP_PROVERS=alt-ergo
+        ;;
+    *)
+        export FRAMAC_WP_PROVERS=alt-ergo
+        ;;
+esac
 set +x
