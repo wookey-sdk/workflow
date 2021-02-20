af338d4d5dc523d05372d4a7778ae1a16f9a90d4
Philippe Thierry <phil@reseau-libre.net>
2021-02-20 10:41:48 +0100

    [update] add USBCDC to CI

---
+++ setenv.libusbcdc.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+FRAMAC_VERSION=`frama-c -version|cut -d'.' -f 1`;
+
+echo "=== sourcing requested paths for Frama-C makefile"
+
+set -x
+export USBOTGHS_API_DIR=../usbotghs/api
+export USBOTGHS_DEVHEADER_PATH=../usbotghs/framac/include
+export LIBSTD_API_DIR=../libstd/api
+export LIBUSB_API_DIR=../libusbctrl/api
+export EWOK_API_DIR=../libstd/framac/includes/ewok
+case $FRAMAC_VERSION in
+    22)
+        export FRAMAC_WP_PROVERS=cvc4,z3
+        ;;
+    21)
+        export FRAMAC_WP_PROVERS=alt-ergo,z3
+        ;;
+    *)
+        export FRAMAC_WP_PROVERS=alt-ergo
+        ;;
+esac
+set +x
